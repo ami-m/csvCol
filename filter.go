793eed9e56195e4135e60bb9d6d47db1930b1506
ami <ami.malimovka@gmail.com>
2020-05-04 12:00:11 +0300

    minor refactoring
    separated the filter functions to a new file

---
+++ filter.go
@@ -0,0 +1,51 @@
+package main
+
+type Filter func(r Record) Record
+
+func buildFilter(p runParams) Filter {
+	if 0 == len(p.cols) {
+		return buildNopFilter()
+	}
+
+	if p.invert {
+		return buildInvertFilter(p)
+	}
+
+	return buildRegularFilter(p)
+}
+
+func buildNopFilter() Filter {
+	return func(r Record) Record {
+		return r
+	}
+}
+
+func buildRegularFilter(p runParams) Filter {
+	return func(r Record) Record {
+		var res Record
+		for _, cell := range p.cols {
+			if len(r) > cell {
+				res = append(res, r[cell])
+			} else {
+				res = append(res, "")
+			}
+		}
+		return res
+	}
+}
+
+func buildInvertFilter(p runParams) Filter {
+	colDic := make(map[int]bool)
+	for _, cell := range p.cols {
+		colDic[cell] = true
+	}
+	return func(r Record) Record {
+		var res Record
+		for cell, col := range r {
+			if !colDic[cell] {
+				res = append(res, col)
+			}
+		}
+		return res
+	}
+}
--- main.go
+++ main.go
@@ -11,7 +11,6 @@ import (
 const workerCount = 500
 
 type Record []string
-type Filter func(r Record) Record
 
 func getCsvReader(r io.Reader) *csv.Reader {
 	return csv.NewReader(r)
@@ -102,54 +101,6 @@ func writeRecordsStream(done <-chan bool, in <-chan Record, w *csv.Writer) {
 	w.Flush()
 	if err := w.Error(); err != nil {
 		log.Fatal(err)
-	}
-}
-
-func buildFilter(p runParams) Filter {
-	if 0 == len(p.cols) {
-		return buildNopFilter()
-	}
-
-	if p.invert {
-		return buildInvertFilter(p)
-	}
-
-	return buildRegularFilter(p)
-}
-
-func buildNopFilter() Filter {
-	return func(r Record) Record {
-		return r
-	}
-}
-
-func buildRegularFilter(p runParams) Filter {
-	return func(r Record) Record {
-		var res Record
-		for _, cell := range p.cols {
-			if len(r) > cell {
-				res = append(res, r[cell])
-			} else {
-				res = append(res, "")
-			}
-		}
-		return res
-	}
-}
-
-func buildInvertFilter(p runParams) Filter {
-	colDic := make(map[int]bool)
-	for _, cell := range p.cols {
-		colDic[cell] = true
-	}
-	return func(r Record) Record {
-		var res Record
-		for cell, col := range r {
-			if !colDic[cell] {
-				res = append(res, col)
-			}
-		}
-		return res
 	}
 }
 
